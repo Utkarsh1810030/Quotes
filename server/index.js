20d8251529b798c90034be70febf9688f7f386ea
Utkarsh1810030 <uchaudhary1234@gmail.com>
2024-03-09 22:44:51 +0530

    server dummy commit

--- server/index.js
+++ server/index.js
@@ -9,20 +9,20 @@ require('./models/User');
 require('./services/passport');
 const keys = require('./config/keys');
 
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, 'client', 'dist', 'index.html'),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   const path = require('path');
+//   app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, 'client', 'dist', 'index.html'),
+//       function (err) {
+//         if (err) {
+//           res.status(500).send(err);
+//         }
+//       }
+//     );
+//   });
+// }
 
 const app = express();
 
